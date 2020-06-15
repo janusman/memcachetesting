9b5b1706370118faa6c672d6d51c5ca0368c6368
Alejandro Garza <alejandro.garza@acquia.com>
2020-06-15 15:52:09 -0400

    Moving around some checks

--- memcache-analyzer.sh
+++ memcache-analyzer.sh
@@ -4,6 +4,7 @@
 tmp="/tmp/memcache-dump.$$"
 tmp2="/tmp/memcache-dump-2.$$"
 tmp_parsed="/tmp/memcache-dump-parsed.$$"
+tmp_parsed_prefix="/tmp/memcache-dump-parsed-prefix.$$"
 tmp_stats="/tmp/memcache-stats.$$"
 
 function show_crosstab() {
@@ -125,20 +126,6 @@ echo "Total memcache items: $num_total"
 #echo "CIDs that are hashes (therefore, can't be analyzed): $num_hashed"
 
 # Parse all items
-# 2 formats:
-#  (A) SLAB=7 ITEM alejandrotest%3Aconfig%3A-core.base_field_override.comment.comment.mail [227 b; 1541892267 s]
-#  (B) SLAB=2 ITEM hercampus_-cache_menu-.wildcard-admin_menu%3A821856%3A [1 b; 0 s]
-#
-# Detect:
-if [ `head -100 $tmp |egrep -c '^SLAB=[0-9][0-9]* ITEM [a-z][a-z0-9_\.]%3A'` -gt 1 ]
-then
-  # Format B
-  cat $tmp |awk -F ' ' '{ slab=substr($1,index($1,"=")+1,2); pos1=index($3, "-"); prefix=substr($3,1,pos1-1); tmp=substr($3,pos1+1); pos2=index(tmp, "-"); bin=substr(tmp, 1, pos2-1); item=substr(tmp, pos2+1); print slab "\t" prefix "\t" bin "\t" item}' >$tmp_parsed
-else
-  # Format A
-  cat $tmp | awk -F ' ' '{ slab=substr($1,index($1,"=")+1,2); pos1=index($3, "%3A"); prefix=substr($3,1,pos1-2); tmp=substr($3,pos1+3); pos2=index(tmp, "%3A"); bin=substr(tmp, 1, pos2-1); item=substr(tmp, pos2+4); print slab "\t" prefix "\t" bin "\t" item}' >$tmp_parsed
-fi
-
 # Example output in $tmp_parsed:
 # { ... snip ... }
 # 10	local.test.sede.sede	config	core.entity_view_mode.config_pages.token
@@ -152,14 +139,78 @@ fi
 # 12	local.test.sede.sede	config	core.entity_view_display.block_content.basic.default
 # 12	local.test.sede.sede	config	core.entity_view_display.paragraph.image.default
 # { ... snip ... }
+function parse_dump() {
+  # 2 formats:
+  #  (A) SLAB=7 ITEM alejandrotest%3Aconfig%3A-core.base_field_override.comment.comment.mail [227 b; 1541892267 s]
+  #  (B) SLAB=2 ITEM hercampus_-cache_menu-.wildcard-admin_menu%3A821856%3A [1 b; 0 s]
+  #
+  # Detect:
+  if [ `head -100 $1 |egrep -c '^SLAB=[0-9][0-9]* ITEM [a-z][a-z0-9_\.]%3A'` -gt 1 ]
+  then
+    # Format B
+    cat $1 |awk -F ' ' '{ slab=substr($1,index($1,"=")+1,2); pos1=index($3, "-"); prefix=substr($3,1,pos1-1); tmp=substr($3,pos1+1); pos2=index(tmp, "-"); bin=substr(tmp, 1, pos2-1); item=substr(tmp, pos2+1); print slab "\t" prefix "\t" bin "\t" item}' #>$tmp_parsed
+  else
+    # Format A
+    cat $1 | awk -F ' ' '{ slab=substr($1,index($1,"=")+1,2); pos1=index($3, "%3A"); prefix=substr($3,1,pos1-2); tmp=substr($3,pos1+3); pos2=index(tmp, "%3A"); bin=substr(tmp, 1, pos2-1); item=substr(tmp, pos2+4); print slab "\t" prefix "\t" bin "\t" item}' #>$tmp_parsed
+  fi
+}
+parse_dump $tmp >$tmp_parsed
+
+
 
 # Number of items by prefix
 #(echo "#items prefix"; cat $tmp | egrep -o "^SLAB=[0-9][0-9]* ITEM [a-z][a-z0-9\.]*" |cut -f3 -d' ' |sort |uniq -c |sort -nr) |column -t
 #echo "" 
 
 show_crosstab $tmp_parsed 2 3 Prefix Bin
-show_crosstab $tmp_parsed 3 1 Cache_Bins Slab
 show_crosstab $tmp_parsed 2 1 Prefix Slab
+
+#
+
+# Get prefixes, but sorted by most-to-least frequent
+#prefixes=`cat $tmp | egrep -o "^SLAB=[0-9][0-9]* ITEM [a-z][a-z0-9\.]*" |cut -f3 -d' ' |sort |uniq -c |sort -nr |awk '{print $2 }'`
+prefixes=`cat $tmp_parsed |cut -f2 |sort |uniq -c |sort -nr |awk '{print $2 }'`
+
+for nom in $prefixes
+do
+
+  echo "== Single Prefix analysis: prefix = $nom =================";
+  echo ""
+
+  grep "SLAB=[0-9][0-9]* ITEM ${nom}" $tmp >$tmp2
+  
+  parse_dump $tmp2 >$tmp_parsed_prefix
+
+  #show_crosstab $tmp_parsed_prefix 2 3 Prefix Bin
+  #show_crosstab $tmp_parsed_prefix 2 1 Prefix Slab
+  show_crosstab $tmp_parsed 3 1 Cache_Bins Slab
+
+  # Figure out the data format...
+  #format=dash
+  #if [ `grep -c "SLAB=[0-9][0-9]* ITEM ${nom}_%3A" $tmp2` -gt 0 ]
+  #then
+  #  format=other
+  #fi
+
+
+  #if [ $format = other ]
+  #then
+  #  (echo "#items cache_bin"; cat $tmp2 | cut -f2 -d% |cut -c3- |sed -e 's/ .*$//g' |sort |uniq -c |sort -nr |head -20) | column -t
+  #else
+  #  (echo "#items cache_bin"; cat $tmp2 | cut -f2 -d- |sed -e 's/ .*$//g' |sort |uniq -c |sort -nr |head -20) | column -t
+  #fi
+
+  #echo ""
+
+  #(echo "#items bin_size"; cat $tmp2|egrep -o '\[[1-9][0-9]* b;' |awk 'function doround(val) { power=1; while (power<val) { power=power*2; } return power; } { size=substr($1,2)+0; bin=doround(size); count[bin]++ } END { for (i in count) { if (count[i]>1) printf("%d %d\n",count[i], i) } }' |sort -nr -t' ' -k2) |column -t
+
+  #echo ""
+
+  echo "Top patterns observed:"
+
+  (echo "# pattern"; cat $tmp2 |awk 'length($0) < 300 { print }' | cut -f2- -d_ |cut -f1 -d' ' | php -r '$result = urldecode(trim(stream_get_contents(STDIN))); print_r($result);' |sed -e 's/\.html_[a-zA-Z0-9_-]\{6,100\}/.html_{hash-value}/g' |sed -e 's/\.html\.twig_[a-zA-Z0-9_-]\{6,100\}/.html.twig_{hash-value}/g' | sed -e 's/[0-9a-f]\{6,100\}/{hex-hash-value}/g' -e 's/:[a-zA-Z0-9_-]*[A-Z][a-zA_Z0-9_-]*/:{hash}/g' | sed -e 's/^views_data:[a-z0-9_]\{2,50\}/views_data:{view-id}/g'  | sed -e 's/^views\.view\.[a-z0-9_]\{2,50\}/views.view.{view-id}/g' |sed -e 's/[0-9][0-9]*/{num}/g' |cut -c2- |sort |uniq -c |sort -nr |head -20) |column -t
+  echo ""
+done
 
 # Get stats
 echo stats slabs |nc localhost 11211 |grep "STAT [0-9]" |tr ':' ' ' |egrep "[^_](chunk_size|chunks_per_page|cmd_set|delete_hits|free_chunks|get_hits|mem_requested|total_chunks|total_pages|used_chunks)[^_]" >$tmp_stats
@@ -170,41 +221,4 @@ echo stats items |nc localhost 11211 |grep "STAT items:[0-9]" |tr ':' ' ' |egrep
 show_crosstab $tmp_stats 4 3 Stats_etc Slab 5
 
 
-# Get prefixes, but sorted by most-to-least frequent
-#prefixes=`cat $tmp | egrep -o "^SLAB=[0-9][0-9]* ITEM [a-z][a-z0-9\.]*" |cut -f3 -d' ' |sort |uniq -c |sort -nr |awk '{print $2 }'`
-prefixes=`cat $tmp_parsed |cut -f2 |sort |uniq -c |sort -nr |awk '{print $2 }'`
-
-for nom in $prefixes
-do
-
-  grep "SLAB=[0-9][0-9]* ITEM ${nom}_" $tmp >$tmp2
-
-  # Figure out the data format...
-  format=dash
-  if [ `grep -c "SLAB=[0-9][0-9]* ITEM ${nom}_%3A" $tmp2` -gt 0 ]
-  then
-    format=other
-  fi
-
-  echo "== Key prefix: $nom =================";
-
-  if [ $format = other ]
-  then
-    (echo "#items cache_bin"; cat $tmp2 | cut -f2 -d% |cut -c3- |sed -e 's/ .*$//g' |sort |uniq -c |sort -nr |head -20) | column -t
-  else
-    (echo "#items cache_bin"; cat $tmp2 | cut -f2 -d- |sed -e 's/ .*$//g' |sort |uniq -c |sort -nr |head -20) | column -t
-  fi
-
-  echo ""
-
-  (echo "#items bin_size"; cat $tmp2|egrep -o '\[[1-9][0-9]* b;' |awk 'function doround(val) { power=1; while (power<val) { power=power*2; } return power; } { size=substr($1,2)+0; bin=doround(size); count[bin]++ } END { for (i in count) { if (count[i]>1) printf("%d %d\n",count[i], i) } }' |sort -nr -t' ' -k2) |column -t
-
-  echo ""
-
-  echo "Top patterns observed:"
-
-  (echo "# pattern"; cat $tmp2 |awk 'length($0) < 300 { print }' | cut -f2- -d_ |cut -f1 -d' ' | php -r '$result = urldecode(trim(stream_get_contents(STDIN))); print_r($result);' |sed -e 's/\.html_[a-zA-Z0-9_-]\{6,100\}/.html_{hash-value}/g' |sed -e 's/\.html\.twig_[a-zA-Z0-9_-]\{6,100\}/.html.twig_{hash-value}/g' | sed -e 's/[0-9a-f]\{6,100\}/{hex-hash-value}/g' -e 's/:[a-zA-Z0-9_-]*[A-Z][a-zA_Z0-9_-]*/:{hash}/g' | sed -e 's/^views_data:[a-z0-9_]\{2,50\}/views_data:{view-id}/g'  | sed -e 's/^views\.view\.[a-z0-9_]\{2,50\}/views.view.{view-id}/g' |sed -e 's/[0-9][0-9]*/{num}/g' |cut -c2- |sort |uniq -c |sort -nr |head -20) |column -t
-  echo ""
-done
-
-#rm $tmp $tmp2 $tmp_parsed
+#rm $tmp $tmp2 $tmp_parsed $tmp_parsed_prefix
